6c2a828044c388fb98f6a341a42e32378a6e0a58
Andrew Chen <andrewchen5678@gmail.com>
2021-11-25 18:57:08 -0800

    clean up

--- src/lib/web3storage_client.ts
+++ src/lib/web3storage_client.ts
@@ -11,10 +11,11 @@ import { FsBlockStore } from 'ipfs-car/blockstore/fs'
 import tmp from 'tmp';
 
 export class Web3StorageClient {
-    private web3storageToken: string;
+    //private web3storageToken: string;
+    private client: Web3Storage;
 
     constructor (web3storageToken: string) {
-        this.web3storageToken = web3storageToken;
+        this.client = this.makeStorageClient(web3storageToken);
       }
     
 
@@ -30,20 +31,9 @@ export class Web3StorageClient {
 
     }
 
-    private getAccessToken(): string {
-        // If you're just testing, you can paste in a token
-        // and uncomment the following line:
-        // return 'paste-your-token-here'
 
-        // In a real app, it's better to read an access token from an
-        // environement variable or other configuration that's kept outside of
-        // your code base. For this to work, you need to set the
-        // WEB3STORAGE_TOKEN environment variable before you run your code.
-        return process.env.WEB3STORAGE_TOKEN || '';
-    }
-
-    private makeStorageClient() {
-        return new Web3Storage({ token: this.getAccessToken() })
+    private makeStorageClient(token: string): Web3Storage {
+        return new Web3Storage({ token })
     }
 
 
@@ -54,9 +44,7 @@ export class Web3StorageClient {
             car = await CarIndexedReader.fromFile(carpath);
             const onStoredChunk = (chunkSize: number) => console.error(`stored chunk of ${chunkSize} bytes`)
 
-
-            const client = this.makeStorageClient();
-            const cid = await client.putCar(car, { name: name,onStoredChunk })
+            const cid = await this.client.putCar(car, { name: name,onStoredChunk })
             console.error('Stored CAR file! CID:', cid);
             console.log(cid);
         }finally{
@@ -86,6 +74,10 @@ export class Web3StorageClient {
                 tmpobj.removeCallback();
             }
         }
+    }
+
+    list(opts?: { before?: string | undefined; maxResults?: number | undefined; } | undefined) {
+        return this.client.list(opts)
     }
 
 }
